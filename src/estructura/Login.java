87cd8169067404f950243537308561a7df1d14a0
Admin <Admin@URIEL-PC>
2025-07-02 00:29:45 -0500

    ya esta quedando

--- src/estructura/Login.java
+++ src/estructura/Login.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.ImageIcon;
 
 public class Login extends JFrame {
 
@@ -52,13 +53,20 @@ public class Login extends JFrame {
 		panel_1.setBounds(0, 0, 205, 461);
 		panel.add(panel_1);
 		
+		JLabel lblNewLabel_2 = new JLabel(".");
+		panel_1.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		panel_1.add(lblNewLabel_1);
+		
 		JPanel panel_2 = new JPanel();
 		panel_2.setBackground(new Color(105, 105, 105));
-		panel_2.setBounds(203, 0, 581, 113);
+		panel_2.setBounds(-32, 0, 579, 113);
 		panel.add(panel_2);
 		
 		JLabel lblNewLabel = new JLabel(".");
-		lblNewLabel.setBounds(660, 0, 124, 461);
+		lblNewLabel.setIcon(new ImageIcon(Login.class.getResource("/Imagenes/banner_lateral.png")));
+		lblNewLabel.setBounds(485, 0, 299, 461);
 		panel.add(lblNewLabel);
 	}
 }
