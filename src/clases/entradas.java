740eb5fbec9df9bf7586e737597360dd23c34ac1
USER <USER@DESKTOP-48BQ9P4>
2025-07-01 22:19:57 -0500

    inciio

--- src/clases/entradas.java
+++ src/clases/entradas.java
@@ -1,11 +1,5 @@
 package clases;
 
 public class entradas {
-	double resultado = 0.0;
-	 String cirocabro;
-	 double suma = 0.1;
-	 int XD;
-	 double profe;
-	 double no_sea_cagon;
-	 double ponganos_20;
+	private int codigo;
 }
